bdccc5976278e2072274abf917e1301e892e299b
Anna <verdyanna@gmail.com>
2016-09-24 16:34:07 +0300

    Template is ready

--- conftest.py
+++ conftest.py
@@ -28,7 +28,7 @@ def app(request):
     web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config['baseUrl'])
-    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
+
     return fixture
 
 
